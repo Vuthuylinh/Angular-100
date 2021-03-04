5e1ce97fdbbbfcdc433eb10fcc34b2546783c926
Linh Vu <thuylinh2490@gmail.com>
2021-03-03 22:22:34 -0500

    feat(app):create server login and signup components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { from } from 'rxjs';
 import { AppComponent } from './app.component';
 
+import {ServerComponent} from './server/server.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component'
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent,
+  ServerComponent,
+  LoginComponent,
+  SignupComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
